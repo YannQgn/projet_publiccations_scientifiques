b4e3f6aa26a7233986d94f021f51ca7c094eca87
YannQgn <yannqueguiner29@hotmail.fr>
2023-08-29 14:13:16 +0200

    commit

--- import.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Importer les données avec mongoimport
-mongoimport --db DBLP --collection publis --file /dblp.json --username root --password example --host mongodb --port 27017
-
-# Attendre indéfiniment
-while true; do
-  sleep 3600  # Attendre 1 heure avant de vérifier à nouveau
-done
